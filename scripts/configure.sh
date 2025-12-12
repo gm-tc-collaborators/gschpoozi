56e4cb154ab8fb7804d9f5da8aa8aa73856e3cf9
gschpoozi <gschpoozi@local>
2025-12-12 18:53:32 +0100

    Fix: load_hardware_state was overwriting wizard-configured serial values with empty strings

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -2103,17 +2103,22 @@ try:
     for key, value in toolboard_assignments.items():
         print(f\"HARDWARE_STATE[toolboard_{key}]='{value}'\")
 
-    # Load MCU serial IDs into WIZARD_STATE
+    # Load MCU serial IDs into WIZARD_STATE (only if not empty - don't overwrite wizard-configured values)
     mcu_serial = data.get('mcu_serial') or ''
     toolboard_serial = data.get('toolboard_serial') or ''
     toolboard_canbus_uuid = data.get('toolboard_canbus_uuid') or ''
     probe_serial = data.get('probe_serial') or ''
     probe_canbus_uuid = data.get('probe_canbus_uuid') or ''
-    print(f\"WIZARD_STATE[mcu_serial]='{mcu_serial}'\")
-    print(f\"WIZARD_STATE[toolboard_serial]='{toolboard_serial}'\")
-    print(f\"WIZARD_STATE[toolboard_canbus_uuid]='{toolboard_canbus_uuid}'\")
-    print(f\"WIZARD_STATE[probe_serial]='{probe_serial}'\")
-    print(f\"WIZARD_STATE[probe_canbus_uuid]='{probe_canbus_uuid}'\")
+    if mcu_serial:
+        print(f\"WIZARD_STATE[mcu_serial]='{mcu_serial}'\")
+    if toolboard_serial:
+        print(f\"WIZARD_STATE[toolboard_serial]='{toolboard_serial}'\")
+    if toolboard_canbus_uuid:
+        print(f\"WIZARD_STATE[toolboard_canbus_uuid]='{toolboard_canbus_uuid}'\")
+    if probe_serial:
+        print(f\"WIZARD_STATE[probe_serial]='{probe_serial}'\")
+    if probe_canbus_uuid:
+        print(f\"WIZARD_STATE[probe_canbus_uuid]='{probe_canbus_uuid}'\")
 
 except Exception as e:
     pass
